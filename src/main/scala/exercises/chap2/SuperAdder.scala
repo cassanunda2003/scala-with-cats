5085c00cb17c6fbd88abe2122201f50d5cc9071d
Cassanunda2003 <mark.reynolds2@gmail.com>
2020-09-07 13:17:59 +0100

    tidy up

--- src/main/scala/exercises/chap2/SuperAdder.scala
+++
@@ -1,9 +0,0 @@
-package exercises.chap2
-
-import cats.Monoid
-
-import cats.syntax.semigroup._ // for |+|
-
-class SuperAdder{
-  def add[A](items: List[A])(implicit monoid: Monoid[A]): A = items.foldRight(monoid.empty)(_ |+| _)
-}
